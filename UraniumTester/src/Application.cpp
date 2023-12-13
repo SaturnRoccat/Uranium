788b2bdb497148de3e9619128cbfad09c28a064b
Duckos-Mods <businwes@hotmail.com>
2023-12-13 09:47:47 +0000

    idk

--- UraniumTester/src/Application.cpp
+++ UraniumTester/src/Application.cpp
@@ -9,10 +9,7 @@
 Application::Application()
 {
 	auto* block = new Uranium::Creation::BlockObject("TestBlock");
-	// Q: Why is this throwing C++ expression must have integral or unscoped enum type?
-	// A: Because you're trying to add a string to a const char*.
 
-	// std::string name = std::string("block") + " (" + DemangleMSVCName(typeid(block).name()).c_str() + ")";
 	std::string name = TOKEN_NICE_NAME(block);
 	Uranium::Logs::Logger::Info("Nice Name {}", name);
 }
