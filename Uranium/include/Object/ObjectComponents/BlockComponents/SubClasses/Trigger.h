de7dbb84b1c5eaf86510fb6d6fe4802705b57a29
Duckos-Mods <businwes@hotmail.com>
2023-12-16 20:43:24 +0000

    Updated queued ticking. Added Trigger.h function to get as json

--- Uranium/include/Object/ObjectComponents/BlockComponents/SubClasses/Trigger.h
+++ Uranium/include/Object/ObjectComponents/BlockComponents/SubClasses/Trigger.h
@@ -21,6 +21,14 @@ namespace Uranium
 						Trigger(std::string condition, std::string event, std::string target)
 							: condition(condition), event(event), target(target) {}
 
+						void getAsJsonData(rapidjson::Value* writeDoc, rapidjson::MemoryPoolAllocator<>& allocator, const std::string& triggerName )
+						{
+							rapidjson::Value trigger(rapidjson::kObjectType);
+							trigger.AddMember("condition", rapidjson::StringRef(this->condition.c_str()), allocator);
+							trigger.AddMember("event", rapidjson::StringRef(this->event.c_str()), allocator);
+							trigger.AddMember("target", rapidjson::StringRef(this->target.c_str()), allocator);
+							writeDoc->AddMember(RJ_STL_S(triggerName), trigger, allocator);
+						}
 					};
 				}
 			}
--- Uranium/src/Object/BlockComponent/queuedTicking.cpp
+++ Uranium/src/Object/BlockComponent/queuedTicking.cpp
@@ -8,11 +8,7 @@ void Uranium::Creation::Components::BlockComponents::queuedTicking::getAsJsonDat
 	queuedTicking.AddMember("looping", this->looping, allocator);
 
 	rapidjson::Value RJ_onTick(rapidjson::kObjectType);
-	RJ_onTick.AddMember("condition", RJ_STL_S(this->onTick.condition), allocator);
-	RJ_onTick.AddMember("event", RJ_STL_S(this->onTick.event), allocator);
-	RJ_onTick.AddMember("target", RJ_STL_S(this->onTick.target), allocator);
-
-	queuedTicking.AddMember("on_tick", RJ_onTick, allocator);
+	this->onTick.getAsJsonData(&RJ_onTick, allocator, "on_tick");
 
 	writeDoc->AddMember("minecraft:queued_ticking", queuedTicking, allocator);
 }
