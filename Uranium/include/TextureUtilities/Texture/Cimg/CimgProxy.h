f40c0591df85687c5ed8a1e9fd954c23247fc383
Duckos-Mods <businwes@hotmail.com>
2023-12-17 10:43:07 +0000

    removed cimg_use_png till i can fix it

--- Uranium/include/TextureUtilities/Texture/Cimg/CimgProxy.h
+++ Uranium/include/TextureUtilities/Texture/Cimg/CimgProxy.h
@@ -1,5 +1,5 @@
 #pragma once
-#define cimg_use_png // Enable PNG support
+// #define cimg_use_png // Enable PNG support
 #include "CImg.h"
 namespace CI = cimg_library;
 typedef CI::CImg<unsigned char> Image;
